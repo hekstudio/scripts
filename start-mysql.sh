37256537b18bf6154ea5f562f37b3391ef0b8598
chuan <brokentransistor@hotmail.com>
2022-04-24 17:46:20 +0800

    update script

--- start-mysql.sh
+++ start-mysql.sh
@@ -6,9 +6,9 @@ docker pull mysql:latest
 # run docker
 docker run \
 -d \
---name=db1 \
+--name=db_test \
 -v /storage/test-mysql/datadir:/var/lib/mysql \
--e MYSQL_ROOT_PASSWORD=23235 \
--e MYSQL_DATABASE=db1 \
+-e MYSQL_ROOT_PASSWORD=123456 \
+-e MYSQL_DATABASE=db_test \
 -p 13306:3306 \
 mysql:latest
